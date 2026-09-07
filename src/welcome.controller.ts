ebadf087854506c85ad0f414818489696c8e9f61
mykelsama <samaniegomike11@gmail.com>
2026-09-07 15:00:53 -0500

    se añadio el welcome controller y el service

---
+++ src/welcome.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from "@nestjs/common";
+import { WelcomeService } from "./welcome.service.js";
+
+@Controller("welcome")
+export class WelcomeController {
+    constructor (private readonly welcomeService: WelcomeService){}
+
+    @Get()
+    getWelcome(): { message: string} {
+        return this.welcomeService.getMessage();
+    }
+}
---
+++ src/welcome.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
+export class WelcomeService {
+    getMessage(): { message: string } {
+        return { message: "Bienvenido a CourseHub API"};
+    }
+}
