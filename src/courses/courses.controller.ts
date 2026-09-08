da7b1e7cc5c12c5f6c00067c9654b5fd1b42a45e
mykelsama <samaniegomike11@gmail.com>
2026-09-08 15:07:27 -0500

    feat: agregar CRUD completo de los courses

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Query} from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query,} from '@nestjs/common';
 import { CoursesService } from './courses.service.js';
 
 @Controller('courses')
@@ -13,5 +13,23 @@ export class CoursesController {
     @Get(':id')
   findOne(@Param('id') id: string) {
     return this.coursesService.findOne(Number(id));
+  }
+
+  @Post()
+  create(@Body() body: { title: string; level: string }) {
+    return this.coursesService.create(body);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id:string,
+    @Body() body: { title?: string; level?: string },
+) {
+    return this.coursesService.update(Number(id), body);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string){
+    return this.coursesService.remove(Number(id));
   }
 }
--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { title } from 'process';
 
 type Course = {
     id: number;
     title: string;
     level: string;
+};
+
+type CreateCourseInput = {
+  title: string;
+  level: string;
+};
+
+type UpdateCourseInput = {
+  title?: string;
+  level?: string;
 };
 
 @Injectable()
@@ -18,11 +29,45 @@ export class CoursesService {
     if (!level) {
       return this.courses;
     }
+
     return this.courses.filter((course) => course.level === level); 
 }
 
 findOne(id: number): Course | undefined {
     return this.courses.find((course) => course.id === id);
     }
+
+
+create(input: CreateCourseInput): Course {
+    const course: Course = {
+      id: Math.max(0, ...this.courses.map((item) => item.id)) + 1,
+      title: input.title,
+      level: input.level,
+    };
+
+    this.courses.push(course);
+    return course;
+  }
+
+  update(id: number, input: UpdateCourseInput): Course | undefined {
+    const course = this.findOne(id);
+
+    if (!course) {
+      return undefined;
+    }
+
+    Object.assign(course, input);
+    return course;
+  }
+
+  remove(id: number): Course | undefined {
+    const index = this.courses.findIndex((course) => course.id === id);
+
+    if (index === -1) {
+      return undefined;
+    }
+
+    const [removedCourse] = this.courses.splice(index, 1);
+    return removedCourse;
+  }
 }
-            
