4154c9d6c61e86de670b513315fb5031e277f1b7
mykelsama <samaniegomike11@gmail.com>
2026-09-08 13:48:11 -0500

    modificacion  para inyeccion en app modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
+import { WelcomeController } from './welcome.controller.js';
+import { WelcomeService } from './welcome.service.js';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, WelcomeController],
+  providers: [AppService, WelcomeService],
 })
 export class AppModule {}
